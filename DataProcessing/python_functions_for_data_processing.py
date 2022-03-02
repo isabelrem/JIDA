e7bc7aaf25cfc9b9e292c1474fea8da29a515e50
isabelrem <42750792+isabelrem@users.noreply.github.com>
2022-03-02 15:39:56 +0000

    Update python_functions_for_data_processing.py

--- DataProcessing/python_functions_for_data_processing.py
+++ DataProcessing/python_functions_for_data_processing.py
@@ -1,15 +1,94 @@
 # use the vcf to snp characteristics table for both
 
+def VCF_to_snp(inVCF, outCSV, start, end):
+    '''converting multiple population VCFs to a combined SNP CSV, removing multi-allelic sites. 
+    
+    Parameters
+    ----------
+    
+    inVCF: str, path to a population VCF for the chromosome.
+    outCSV: str, path to output CSV containing variant information for the chromosome.
+    start: int, index at which to start the subset dataframe
+    end: int, index at which to end the subset dataframe
+    
+    
+    Description
+    -----------
+    
+    Receives 4 parameters: the path to a VCF, the path to an output CSV, a list of population codes and 
+     start and end index values.
+    Converts the VCF to a dataframe, and then subsets that dataframe based on the start and end indices specified.
+    Once subset, multiallelic sites are removed from the dataframe.
+    The columns are then reduced to just 'chromsome','position','rs_value','reference','alternate'.
+    The resultant dataframe is then written to a CSV, using the path specified by outCSV and the start and end indices.
+    Converting the VCF to a dataframe uses the pre-written function VCFtoPandas().
+    This function depends on the python packages/modules pandas, os, pathlib
+    
+    
+    Returns
+    -------
+    One csv file with the columns 'chromsome','position','rs_value','reference','alternate', for the range specified by the start and end parameters.
+    
+    '''
 
-# setting directory
-import os
-newpath = "E:\Bioinf_data\population VCFs\snp characteristics"
-os.chdir(newpath)
-path = os.getcwd()
-print(path)
+    # import dependencies
+    import pandas as pd
+    import os
+    from pathlib import Path
+
+    # iterate over the 5 population vcfs
+    for inVCF, outCSVin zip(inputlist,outputlist):
+
+        # check inputs
+        print("process started...")
+        print(inVCF)
+
+        # create dataframe
+        df = VCFtoPandas(inVCF)
+        print("dataframe created")
+
+        # rename columns in accordance with SQL schema
+        df.rename(columns={'#CHROM': 'chromosome', 'POS': 'position', 'ID': 'rs_value', 'REF': 'reference', 'ALT': 'alternate'}, inplace=True)
+
+        # shorten the df
+        df = df.loc[start:end]
+
+        # extract row names
+        row_labels_df = list(df.index)
+
+        # remove multiallelic sites
+        for row in row_labels_df:
+            alt = df.loc[row,'alternate']
+            alt = len(alt)
+            if alt != 1:
+                # delete multiallelic row from dataframe
+                df = df.drop(labels=row)
+
+        print("multi allelic sites removed")
+        df = df.reset_index()
+        print("index reset")
+
+        
+        # extract updated row names
+        row_labels_df = list(df.index)
+
+
+        # remove unwanted columns
+        df_info = df.filter(['rs_value'], axis=1)
+
+        print("data filtered")
+
+
+        # filter dataframe to just the desired columns
+        df = df.filter(['chromsome','position','rs_value','reference','alternate'])
+        
+        df.to_csv(outCSV, index = False)
+        
+
+
 
 def VCF_to_snp_characteristics(inputlist, outputlist, popcodelist, start, end):
-    'converting multiple population VCFs to a combined SNP characterisitcs CSV, removing multi-allelic sites. Author: Isabel Thompson.'
+    '''converting multiple population VCFs to a combined SNP characterisitcs CSV, removing multi-allelic sites. Author: Isabel Thompson.'''
 
     # import dependencies
     import pandas as pd
